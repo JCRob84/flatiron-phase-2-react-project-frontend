446958c3fbe5c4c71c0a3718024aacf13be15cf9
Juan Robledo <robledoc@gmail.com>
2023-10-07 18:03:14 -0500

    change footer title

---
+++ src/components/Footer.jsx
@@ -0,0 +1,60 @@
+const Footer = () => {
+  return (
+    <>
+      <footer>
+        <nav>
+          <ul>
+            <li>
+              <a href="#top">Go to top</a>
+            </li>
+            <li>
+              <a href="#" target="_blank">
+                Aerobic exercise
+              </a>
+              <p>
+                Aerobic exercise is any type of cardiovascular conditioning that
+                involves the larger muscles with rhythmic and repetitive
+                movements. Aerobic exercises make the heart, lungs, blood
+                vessels, and muscles work more efficiently. They also increase
+                overall strength and endurance. Aerobic exercise also lifts your
+                mood, helps you sleep better.
+              </p>
+            </li>
+            <li>
+              <a href="#" target="_blank">
+                Flexibility exercises
+              </a>
+              <p>
+                lexibility exercises, also known as range of motion or
+                stretching exercises, improve muscle elasticity and joint range
+                of motion. You can expect to feel a comfortable tension when
+                doing flexibility exercises, but never pain. Having good
+                flexibility can help you perform virtually any movement more
+                comfortably, from walking to sitting or bending to lift
+                something. Flexibility exercises can also help reduce stress and
+                promote relaxation. They also assist in decreasing stiffness and
+                postural changes that may sometimes occur afterward.
+              </p>
+            </li>
+            <li>
+              <a href="#" target="_blank">
+                Stregth or endurance exercises
+              </a>
+              <p>
+                Strength exercises, also known as resistance exercises, make the
+                muscles work harder by adding weight or resistance to the
+                movement. Strength exercises also strengthen bones, improve
+                balance, posture, and overall quality of life, as household
+                tasks and recreational activities become easier and more
+                enjoyable. After breast cancer surgery, strength exercises can
+                also help improve muscle balance or strengthen muscles.
+              </p>
+            </li>
+          </ul>
+        </nav>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;

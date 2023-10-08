54c85517551ba1306551d9843e0738da023d0fad
Juan Robledo <robledoc@gmail.com>
2023-10-08 01:37:48 -0500

    modified component Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,23 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../App.css";
+
 const Header = () => {
+  const handleButtonClick = (section) => {
+    console.log(`Switching to ${section} section`);
+  };
   return (
-    <>
-      <header>
-        <h1>Exercise Tracker</h1>
-        <nav>
-          <ul>
-            <li>
-              <a href="#home">Home</a>
-            </li>
-            <li>
-              <a href="#exercises">Exercises</a>
-            </li>
-            <li>
-              <a href="#add-exercise">Add Exercise</a>
-            </li>
-          </ul>
-        </nav>
-      </header>
-    </>
+    <header>
+      <h1>Exercise Tracker</h1>
+      <nav>
+        <div class="w3-bar w3-black">
+          <Link className="w3-bar-item w3-button" to="/">
+            Home
+          </Link>
+          <Link className="w3-bar-item w3-button" to="/exercise">
+            Exercises
+          </Link>
+          <Link className="w3-bar-item w3-button" to="/excerciseManager">
+            Add Exercise
+          </Link>
+        </div>
+      </nav>
+    </header>
   );
 };
 
